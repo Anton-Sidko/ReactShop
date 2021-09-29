3c7b201f973cc1199d1716ca8b7f6e7e46e5ec21
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2021-09-29 12:48:55 +0300

    Minor fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return <nav className="indigo lighten-2">
         <div className="nav-wrapper">
-            <a href="!#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="https://github.com/Anton-Sidko/ReactShop">Repo</a></li>
             </ul>
